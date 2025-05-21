cb78f50f89a939a9bf8ee865a01931208f874462
Anwesha Saha <anwesha@bu.edu>
2025-05-21 12:55:59 -0400

    updated nonexistent hash

--- thirdparty/learned_hashing.cmake
+++ thirdparty/learned_hashing.cmake
@@ -4,7 +4,7 @@ set(LEARNED_HASHING_LIBRARY learned-hashing)
 FetchContent_Declare(
   ${LEARNED_HASHING_LIBRARY}
   GIT_REPOSITORY https://github.com/DominikHorn/learned-hashing.git
-  GIT_TAG 8c5fdc4
+  GIT_TAG main
   )
 
 FetchContent_MakeAvailable(${LEARNED_HASHING_LIBRARY})
